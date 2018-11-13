111cdc345adfa733a0fd108365b83ab65076b551
qddlgg <2545790899@qq.com>
2018-11-13 23:17:14 +0800

    操作1

---
+++ gitTest/src/main/java/qddlgg.java
@@ -0,0 +1,8 @@
+public class qddlgg {
+    public static void main(String[] args) {
+        getHelloworld();
+    }
+    public static void getHelloworld(){
+        System.out.println("hello world");
+    }
+}
